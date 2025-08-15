729d38514f13c624f2d1d6d133cda470f8907975
mwahajjutt123@gmail.comgit config --global user.email mwahajjutt123@gmail.com <mwahajjutt123@gmail.com>
2025-08-15 20:28:01 +0500

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -1,18 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+
+// function to change value of x
+void changeA(int *a)
+{
+    *a = 30; // dereference pointer to change value
+}
 
 int main()
 {
-    //pointers are variables that store the address of another variable.
+    // pointers are variables that store the address of another variable.
     int a = 10;
-    int *p = &a; // p is a pointer that stores the address of variable a]
+    int *p = &a;                         // p is a pointer that stores the address of variable a]
     cout << "Value of a: " << p << endl; // prints 10
     cout << "Address of a: " << &a << endl;
 
-
-    //pointer to pointer is a variable that stores the address of another pointer variable.
-    int **q = &p;   
+    // pointer to pointer is a variable that stores the address of another pointer variable.
+    int **q = &p;
     cout << "Address of pointer p: " << &p << endl; // prints address of pointer p
-    cout << "Value of pointer q: " << *q << endl; // prints value of pointer q
+    cout << "Value of pointer q: " << *q << endl;   // prints value of pointer q
+
+    // example
+
+    int b = 5;
+    int *ptr = &b;       // p is a pointer to a
+    int **parptr = &ptr; // q is a pointer to pointer p
+
+    cout << *(&b) << endl;
+    cout << *(&ptr) << endl;
+    cout << **parptr << endl;
+    cout << ptr << endl;
+    cout << *parptr << endl;
+
+    // pass by refrence
+
+    int x = 20;
+    changeA(&x);
+    cout << "changed value of x: " << x << endl;
+
     return 0;
 }
